8be28ada72de1a8a4f4639a905754907cf1d42c4
kale-z <khaledsaleh@std.sehir.edu.tr>
2015-12-22 07:49:41 +0200

    Initial commit

---
+++ models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=40)
+    email = models.EmailField()
+
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=40)
+    office = models.CharField(max_length=40)
+    phone = models.CharField(max_length=40)
+    email = models.EmailField()
+
+class Course(models.Model):
+    name = models.CharField(max_length=30)
+    classroom = models.CharField(max_length=50)
+    code = models.CharField(max_length=60)
+    time = models.CharField(max_length=50)
+    teacher = models.ForeignKey(Teacher)
+    student = models.ManyToManyField(Student)
